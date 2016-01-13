23ebe9f5b56c1c7bdef18fc5c917a89d4ba4377e
Ryeland Gongora <rsgmon@gmail.com>
2016-01-12 17:34:19 -0800

    angular delivering data

--- public/app/app.js
+++ public/app/app.js
@@ -3,9 +3,5 @@ var app = angular.module('app',['ngRoute', 'ngResource']);
 app.config(function ($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
-        .when('/', { templateUrl: '/partials/main/', controller: 'mainCtrl' })
+        .when('/', { templateUrl: '/partials/main/', controller: 'fmMainCtrl' })
 });
-
-app.controller('mainCtrl', function($scope) {
-    $scope.myVar = "This sucks."
-})
---
+++ public/app/main/fmMainCtrl.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Rye on 1/12/2016.
+ */
+app.controller('fmMainCtrl', function($scope) {
+    $scope.sample_portfolio = [
+        {symbol: "spy", weight: "20%" },
+        {symbol: "spy", weight: "20%" }
+    ]
+});
